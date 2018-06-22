8ff3999bf33dcb50e7b27e713f6b407da0a76b1e
Kerry Rodden <kerryrodden@users.noreply.github.com>
2018-06-22 10:45:36 -0700

    fix: more guards and logging for service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // https://developers.google.com/web/fundamentals/primers/service-workers/
 // https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 
-const currentCacheName = 'mws-restaurants-v1';
+const currentCacheName = 'restaurant-reviews-20180622';
 
 // Cache page content on install.
 self.addEventListener('install', function (event) {
@@ -30,6 +30,8 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(currentCacheName).then(function (cache) {
       return cache.addAll(urlsToCache);
+    }).catch(function (error) {
+      console.log('Service worker error when adding URLs to cache:', error);
     }));
 });
 
@@ -44,15 +46,23 @@ self.addEventListener('activate', function (event) {
           }
         })
       );
+    }).catch(function (error) {
+      console.log('Service worker error when deleting old caches:', error);
     })
   );
 });
 
 // Serve requests from the cache first, then from the network.
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+  const url = new URL(event.request.url);
+  // Don't try to intercept anything dynamic from the Google Maps API.
+  if (url.hostname !== 'maps.googleapis.com') {
+    event.respondWith(
+      caches.match(event.request, {ignoreSearch: true}).then(function (response) {
+        return response || fetch(event.request);
+      }).catch(function (error) {
+        console.log('Service worker error when fetching:', error);
+      })
+    );
+  }
 });
